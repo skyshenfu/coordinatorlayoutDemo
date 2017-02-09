ede8c63a671205ce288a2f690b34b26309a190d1
painworkpc <zhangty@all-help.com>
2017-02-09 14:03:13 +0800

    完成Demo

--- CoordinatorTest/app/src/main/java/com/pain/coordinatortest/MainActivity.java
+++ CoordinatorTest/app/src/main/java/com/pain/coordinatortest/MainActivity.java
@@ -1,22 +1,32 @@
 package com.pain.coordinatortest;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
-
+//activity_main1为图片折叠后变成toolbar的效果
+//activity_main2为颜色折叠后渐变效果
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     LangAdapter adapter;
+    Toolbar toolbar;
+    CollapsingToolbarLayout collapsingToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         recyclerView= (RecyclerView) findViewById(R.id.recycle_view);
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        collapsingToolbar= (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         adapter=new LangAdapter(this);
+        setSupportActionBar(toolbar);
+        collapsingToolbar.setTitle("122");
+        collapsingToolbar.setContentScrimColor(0xffFF4081);
         adapter.setListener(new LangViewHolder.OnEventListener() {
             @Override
             public void onClick(View view, int position) {
